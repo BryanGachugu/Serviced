363547e95335ba85c7f14e21e6a8d4e7f76996d1
bryan <briangachugu5@gmail.com>
2021-04-20 19:40:35 +0300

    2021-04-20 19:40

--- app/src/main/java/com/gachugusville/development/serviced/Adapters/ProviderRecyclerAdapter.java
+++ app/src/main/java/com/gachugusville/development/serviced/Adapters/ProviderRecyclerAdapter.java
@@ -1,6 +1,55 @@
 package com.gachugusville.development.serviced.Adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ProviderRecyclerAdapter extends RecyclerView.Adapter<SearchedProviders.ViewHolder> {
+import com.gachugusville.development.serviced.R;
+import com.gachugusville.development.serviced.Utils.Provider;
+import com.google.android.material.card.MaterialCardView;
+
+import java.util.List;
+
+public class ProviderRecyclerAdapter extends RecyclerView.Adapter<ProviderRecyclerAdapter.SearchedProviderViewHolder> {
+
+    List<Provider> searchList;
+    Context context;
+
+    public ProviderRecyclerAdapter(List<Provider> searchList, Context context) {
+        this.searchList = searchList;
+        this.context = context;
+    }
+    
+
+    @NonNull
+    @Override
+    public SearchedProviderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.search_results, parent, false);
+        return new SearchedProviderViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SearchedProviderViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return searchList.size();
+    }
+
+    public static class SearchedProviderViewHolder extends RecyclerView.ViewHolder {
+
+        public SearchedProviderViewHolder(@NonNull View itemView) {
+            super(itemView);
+           
+        }
+    }
+
 }
