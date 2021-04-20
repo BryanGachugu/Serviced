4d2a35bb541516828d71a06c8d5dfd1fd8602bdf
bryan <briangachugu5@gmail.com>
2021-04-20 16:40:34 +0300

    2021-04-20 16:40

--- app/src/main/java/com/gachugusville/development/serviced/User/SearchActivity.java
+++ app/src/main/java/com/gachugusville/development/serviced/User/SearchActivity.java
@@ -3,31 +3,26 @@ package com.gachugusville.development.serviced.User;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import com.gachugusville.development.serviced.Adapters.CustomSearchAdapter;
 import com.gachugusville.development.serviced.R;
 
-public class SearchFragment extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity {
     private ListView listView;
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.search_fragment, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_fragment);
 
-        listView = view.findViewById(R.id.listView);
+        listView = findViewById(R.id.listView);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
@@ -36,25 +31,24 @@ public class SearchFragment extends AppCompatActivity {
                 //execution come here when an item is clicked from
                 //the search results displayed after search form is submitted
                 //you can continue from here with user clicked search item
-                Toast.makeText(getContext(),
+                Toast.makeText(SearchActivity.this,
                         "clicked search result item is" + ((TextView) view).getText(),
                         Toast.LENGTH_SHORT).show();
             }
         });
         // search
         handleSearch();
-        return view;
     }
-
-}
 
     @Override
     protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
         setIntent(intent);
         handleSearch();
     }
+
     private void handleSearch() {
-        Intent intent = getActivity().getIntent();
+        Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String searchQuery = intent.getStringExtra(SearchManager.QUERY);
 
@@ -63,11 +57,11 @@ public class SearchFragment extends AppCompatActivity {
                     StoresData.filterData(searchQuery));
             listView.setAdapter(adapter);
 
-        }else if(Intent.ACTION_VIEW.equals(intent.getAction())) {
-            String selectedSuggestionRowId =  intent.getDataString();
+        } else if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+            String selectedSuggestionRowId = intent.getDataString();
             //execution comes here when an item is selected from search suggestions
             //you can continue from here with user selected search item
-            Toast.makeText(getContext(), "selected search suggestion "+selectedSuggestionRowId,
+            Toast.makeText(this, "selected search suggestion " + selectedSuggestionRowId,
                     Toast.LENGTH_SHORT).show();
         }
     }
