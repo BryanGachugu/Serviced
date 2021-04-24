d86046d4218c085a757585d27c66d5a14f8df1f7
bryan <briangachugu5@gmail.com>
2021-04-24 11:05:28 +0300

    2021-04-24 11:05

---
+++ app/src/main/java/com/gachugusville/development/serviced/User/ProviderDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.gachugusville.development.serviced.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProviderDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provider_details);
+    }
+}
